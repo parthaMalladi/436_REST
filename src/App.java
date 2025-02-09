ddc38c09743dc887eaaa4e12f33fb271dfd0f9d3
parthaMalladi <partha.malladi@gmail.com>
2025-02-09 22:41:23 +0000

    complete

--- src/App.java
+++ src/App.java
@@ -8,56 +8,52 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-        String link = "";
-
-        while (true) {
-            System.out.print("\nEnter City Name (Enter 'Quit' to Exit): ");
-            input = scanner.nextLine();
-            String output;
-
-            if (input.equalsIgnoreCase("quit")) {
-                break;
-            }
-
-            // parameters to get weather data
-            String[][] weatherHeaders = {};
-            link = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&APPID=ff7f22463aef23ef6d3d620fb0868653";
-            output = getInfo(link, weatherHeaders, "temp");
-            if (output.equals("invalid")) {
-                System.out.println("Invalid Name or Error, please re-enter the city name");
-                continue;
-            }
-            double temp = Double.parseDouble(output) - 273.15;
-            System.out.println("The temperature in " + input + " is " + String.format("%.2f", temp) + " degrees Celcius.");
-
-            // parameters to get population data
-            String[][] populationHeaders = {
-                {"X-Api-Key", "0xSuV9jLP2qCg85M2Yw7CA==abxpVXafPWf6pZy6"},
-                {"accept", "application/json"}
-            };
-            link = "https://api.api-ninjas.com/v1/city?name=" + input;
-            output = getInfo(link, populationHeaders, "population");
-            if (output.equals("invalid")) {
-                System.out.println("Invalid Name or Error, please re-enter the city name");
-                continue;
-            }
-            System.out.println("The population of " + input + " is " + output.substring(1) + " people.");
-
-            // parameters to get country
-            String[][] countryHeaders = {
-                {"x-rapidapi-key", "feb8505df7mshc819e40e5708dbdp159311jsn5e5d142d62df"},
-                {"Accept", "application/json"}
-            };
-            link = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=" + input + "&limit=1&sort=-population";
-            output = getInfo(link, countryHeaders, "country");
-            if (output.equals("invalid")) {
-                System.out.println("Invalid Name or Error, please re-enter the city name");
-                continue;
-            }
-            System.out.println(input + " is located in " + output.substring(1, output.length() - 1));
+        if (args.length != 1) {
+            System.out.println("Usage: java App <city name>");
+            return;
         }
+
+        String input = args[0];
+        String output;
+        String link;
+
+        // parameters to get weather data
+        String[][] weatherHeaders = {};
+        link = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&APPID=ff7f22463aef23ef6d3d620fb0868653";
+        output = getInfo(link, weatherHeaders, "temp");
+        if (output.equals("invalid")) {
+            System.out.println("Invalid Name or Error, please re-enter the city name\n");
+            return;
+        }
+        double temp = Double.parseDouble(output) - 273.15;
+        System.out.println("The temperature in " + input + " is " + String.format("%.2f", temp) + " degrees Celcius.");
+
+        // parameters to get population data
+        String[][] populationHeaders = {
+            {"X-Api-Key", "0xSuV9jLP2qCg85M2Yw7CA==abxpVXafPWf6pZy6"},
+            {"accept", "application/json"}
+        };
+        link = "https://api.api-ninjas.com/v1/city?name=" + input;
+        output = getInfo(link, populationHeaders, "population");
+        if (output.equals("invalid")) {
+            System.out.println("Invalid Name or Error, please re-enter the city name\n");
+            return;
+        }
+        System.out.println("The population of " + input + " is " + output.substring(1) + " people.");
+
+        // parameters to get country
+        String[][] countryHeaders = {
+            {"x-rapidapi-key", "feb8505df7mshc819e40e5708dbdp159311jsn5e5d142d62df"},
+            {"Accept", "application/json"}
+        };
+        link = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=" + input + "&limit=1&sort=-population";
+        output = getInfo(link, countryHeaders, "country");
+        if (output.equals("invalid")) {
+            System.out.println("Invalid Name or Error, please re-enter the city name\n");
+            return;
+        }
+        System.out.println(input + " is located in " + output.substring(1, output.length() - 1));
+        System.out.println();
     }
 
     private static String getInfo(String link, String[][] headers, String key) {
---
+++ src/runit.sh
@@ -0,0 +1,15 @@
+# call "./runit.sh" in terminal after cd <src folder>
+
+javac --release 8 App.java
+
+# TO RUN CODE
+# java App <city name>
+
+# Examples
+java App London
+java App Dubai
+java App Lisbon
+
+# Error testing
+java App fkladjkaj
+java App Toronto
